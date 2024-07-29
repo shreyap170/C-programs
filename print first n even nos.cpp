fa856241636ee82342a548a6658d5588bb1e6a8a
Shreya Pachghare <137901200+shreyap170@users.noreply.github.com>
2024-07-29 20:19:59 +0530

    Add files via upload

---
+++ print first n even nos.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,count;
+	cout<<"enter num: ";
+	cin>>n;
+	
+	for(count=2 ;count<=n ;count=count+2)
+	{
+		cout<<count<<endl;
+	}
+	
+};
