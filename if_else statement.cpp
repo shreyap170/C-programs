7fb6a53fe1ac6bd341dcc8e28244fb89d9d7b621
Shreya Pachghare <137901200+shreyap170@users.noreply.github.com>
2024-07-29 20:18:53 +0530

    Add files via upload

---
+++ if_else statement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int package;
+	cout<<"enter your package:";
+	cin>>package;
+	
+	if(package>10)
+	{
+		cout<<"accepted";
+	}
+	else
+	{
+		cout<<"rejected";
+	}
+};
